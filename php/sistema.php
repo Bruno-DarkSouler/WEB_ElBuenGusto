8c968266cb96c3b786b9f818041b4d25fd62885d
Bruno-DarkSouler <brunofornasar@gmail.com>
2025-09-11 14:29:04 -0300

    funcio para Insert en sistema php

--- php/sistema.php
+++ php/sistema.php
@@ -2,6 +2,11 @@
     session_start();
 
     $conexion = new mysqli("localhost", "root", "", "calera");
+    $conexion->autocommit(FALSE); //Desactiva el autocommit que efectua una consulta ni bien se realiza
+
+    function errorFatal(){
+        echo "Error fatal";
+    }
 
     function consultaSelect(&$conexion, $consulta, $tipo_d, $parametros){
         $cursor = $conexion->prepare($consulta);
@@ -20,7 +25,23 @@
         }else{
             return 1;
         }
+    } //Devuelve un array de indices numericos que contien arrays con indices alfanumericos, siendo cada uno de estos un registro de la base de datos y los indices alfanumericos las columnas de la base de datos
+
+    function consultaInsert(&$conexion, $consulta, $tipo_d, $parametros){
+        $cursor = $conexion->prepare($consulta);
+        $cursor->bind_param($tipo_d, ...$parametros);
+
+        $cursor->execute();
+        $resultado = $cursor->get_result();
+
+        if(!$conexion->commit()){
+            $conexion->rollback();
+            errorFatal();
+        }
+
+        return $resultado;
     }
 
-    print_r(consultaSelect($conexion, "SELECT * FROM usuarios WHERE nombre = ? AND id <= ?;", "si", ["Bruno", 7]));
+    print_r(consultaSelect($conexion, "SELECT * FROM usuarios WHERE nombre = ? AND permisos = ?;", "ss", ["Bruno", "PR"]));
+    // print_r(consultaInsert($conexion, "INSERT INTO `usuarios`(`nombre`, `permisos`) VALUES (?, ?);", "ss", ["Bruno", "PR"]));
 ?>
